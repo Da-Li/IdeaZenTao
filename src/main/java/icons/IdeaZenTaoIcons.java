602cdec335c4b82fb459f1a50e9e2afb22fb1abd
liyu <ly@flytone.cn>
2018-10-10 10:19:20 +0800

    秀太Icon类

---
+++ src/main/java/icons/IdeaZenTaoIcons.java
@@ -0,0 +1,28 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+import com.js_ku.zentao.IdeaZenTao;
+
+import javax.swing.*;
+
+public final class IdeaZenTaoIcons {
+
+     public static final String theme = IdeaZenTao.getTheme();
+     public static final Icon NO_LOGIN = IconLoader.getIcon(theme +"/unlogin.png");
+     public static final Icon WEB = IconLoader.getIcon(theme +"/web.png");
+
+     public static Icon getIconByBugSize(int bugSize){
+
+          if (bugSize == 0){
+               return IconLoader.getIcon(IdeaZenTao.getTheme()+"nonBug.png");
+          }
+
+          if (bugSize > 9){
+               return IconLoader.getIcon("");
+          }
+
+          return IconLoader.getIcon(IdeaZenTao.getTheme()+"number/number_"+bugSize+".png");
+     }
+
+
+}
